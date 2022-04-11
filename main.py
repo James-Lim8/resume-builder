60e17a85266a23b46899175b0bb27df195a83839
James-Lim8 <89911696+James-Lim8@users.noreply.github.com>
2022-04-12 02:53:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from docxtpl import DocxTemplate
 st.write("# Resume Builder")
 
 st.write("Your particulars!")
-
+#DEFINE THE DICTIONARY
 contact = {
     "name": "",
     "email": "",
@@ -16,9 +16,14 @@ contact = {
     "soc_skills": [],
     "pt_skills": [],
     "other_skills": [],
-    "projects": []
+    "projects": [],
+    "experiences": [],
+    "educations": [],
+    "certifications": [],
+    "additional_infos": []
 }
 
+#PERSONAL PARTICULARS
 user_name = st.text_input('Enter your full name: ', placeholder=" Full Name")
 user_email = st.text_input('Enter your email: ', placeholder=" Email")
 user_nationality = st.text_input('Enter your nationality: ', placeholder=" Nationality")
@@ -26,6 +31,7 @@ user_phone = st.text_input('Enter your phone number: ', placeholder=" Phone Numb
 user_github = st.text_input('Enter your github link, otherwise put N.A.', placeholder=" Github Link")
 user_career_obj = st.text_area('Describe your career objectives: ', placeholder=" Career Objectives")
 
+#ADDING TO RESPECTIVE DICTIONARY KEYS
 contact["name"] = user_name
 contact["email"] = user_email
 contact["nationality"] = user_nationality
@@ -33,54 +39,131 @@ contact["phone"] = user_phone
 contact["github"] = user_github
 contact["career_objective"] = user_career_obj
 
-#End of getting user particulars
+#[*]--------------------------------------------------------------------------------------------[*]
 
-# print('Moving to your Skillsets')
+#SKILLS
 
-
-number_of_basic_skills=st.slider('How many Basics & Networking skills would you like to input?',0,20)
-for each_basic_skill in range(number_of_basic_skills):
-    each_basic_skill = st.text_input("Enter the list of skills you learnt from Basics & Networking: ", placeholder=" Linux commands")
+number_of_basic_skills=st.number_input('How many Basics & Networking skills would you like to input? (Up to 20)',min_value=0,max_value=20)
+for each_basic_skill in range(int(number_of_basic_skills)):
+    each_basic_skill = st.text_input(label="Enter the list of skills you learnt from Basics & Networking: ", key=each_basic_skill, placeholder=" Linux commands")
     contact["basic_skills"].append(each_basic_skill)
 
-number_of_soc_skills=st.slider('How many SOC skills would you like to input?',0,20)
-for each_soc_skill in range(number_of_soc_skills):
-    each_soc_skill = st.text_input("Enter the list of skills you learnt from SOC: ", placeholder=" ELK")
+number_of_soc_skills=st.number_input('How many SOC skills would you like to input? (Up to 20)',min_value=0,max_value=20)
+for each_soc_skill in range(int(number_of_soc_skills)):
+    each_soc_skill = st.text_input("Enter the list of skills you learnt from SOC: ", key=each_soc_skill, placeholder=" ELK")
     contact["soc_skills"].append(each_soc_skill)
 
-number_of_pt_skills=st.slider('How many Pen-Test skills would you like to input?',0,20)
-for each_pt_skill in range(number_of_pt_skills):
-    each_pt_skill=st.text_input("Enter the list of skills you learnt from PT: ", placeholder=" msfconsole")
+number_of_pt_skills=st.number_input('How many Pen-Test skills would you like to input? (Up to 20)',min_value=0,max_value=20)
+for each_pt_skill in range(int(number_of_pt_skills)):
+    each_pt_skill=st.text_input("Enter the list of skills you learnt from PT: ", key=each_pt_skill, placeholder=" msfconsole")
     contact["pt_skills"].append(each_pt_skill)
 
-number_of_other_skills=st.slider('How many Other skills would you like to input?',0,20)
-for each_other_skill in range(number_of_other_skills):
-    each_other_skill=st.text_input("Enter the list of skills from other areas: ", placeholder=" photoshop / SPSS/ accounting etc.")
+number_of_other_skills=st.number_input('How many Other skills would you like to input? (Up to 20)',min_value=0,max_value=20)
+for each_other_skill in range(int(number_of_other_skills)):
+    each_other_skill=st.text_input("Enter the list of skills from other areas: ", key=each_other_skill,placeholder=" photoshop / SPSS/ accounting etc.")
     contact["other_skills"].append(each_other_skill)
 
+#[*]--------------------------------------------------------------------------------------------[*]
 
-#End of getting the skills input
-
-print('Moving on to the projects section. Please provide details of your projects starting from the most recent one')
-
-n=int(input("How many projects information would you like to key in?: "))
+#PROJECTS
+proj_n=st.number_input("How many projects information would you like to key in?: ",min_value=0,max_value=20)
 
 all_projects=[]
 
-for number_of_projects in range(n):
+for number_of_projects in range(proj_n):
     each_proj={}
-    proj_title=input("Please provide the title of the project: ")
-    proj_date=input("Please provide the Start date of the project: ")
-    proj_content=input("Please provide the content of the project: ")
-    contact["pt_skills"].append(proj_title)
-    each_proj["date"] = proj_date
+    proj_title=st.text_input("Please provide the title of the project: ", key=number_of_projects, placeholder="Project Title")
+    proj_start_date=st.text_input("Please provide the Start date of the project in (MM/YYYY): ", key=number_of_projects, placeholder="Start Date")
+    proj_content=st.text_area("Please provide the content of the project: ", key=number_of_projects, placeholder="Project Details")
+    each_proj["title"] = proj_title
+    each_proj["date"] = proj_start_date
     each_proj["content"] = proj_content
     all_projects.append(each_proj)
 
 contact["projects"]=all_projects
 
-# #print(contact)
+#[*]--------------------------------------------------------------------------------------------[*]
 
-# doc = DocxTemplate("template-cv.docx")
-# doc.render(contact)
-# doc.save("test-template.docx")
+#EXPERIENCES
+exp_n=st.number_input("How many experiences information would you like to key in?: ",min_value=0,max_value=20)
+
+all_exps=[]
+
+for number_of_exps in range(exp_n):
+    each_exp={}
+    exp_title=st.text_input("Please provide the job title ,followed by company name: ", key=number_of_exps, placeholder="Project Title")
+    exp_start_date=st.text_input("Please provide the Start - end date of the project in (month year - month year): ", key=number_of_exps, placeholder="Start Date")
+    exp_content=st.text_area("Please provide the content of the work experience: ", key=number_of_exps, placeholder="Project Details")
+    each_exp["title"] = exp_title
+    each_exp["date"] = exp_start_date
+    each_exp["content"] = exp_content
+    all_exps.append(each_exp)
+
+contact["experiences"]=all_exps
+
+#[*]--------------------------------------------------------------------------------------------[*]
+
+#EDUCATIONS
+n=st.number_input("How many projects information would you like to key in?: ",min_value=0,max_value=20)
+
+all_projects=[]
+
+for number_of_projects in range(n):
+    each_proj={}
+    proj_title=st.text_input("Please provide the title of the project: ", key=number_of_projects, placeholder="Project Title")
+    proj_start_date=st.text_input("Please provide the Start date of the project in (MM/YYYY): ", key=number_of_projects, placeholder="Start Date")
+    proj_content=st.text_area("Please provide the content of the project: ", key=number_of_projects, placeholder="Project Details")
+    each_proj["title"] = proj_title
+    each_proj["date"] = proj_start_date
+    each_proj["content"] = proj_content
+    all_projects.append(each_proj)
+
+contact["projects"]=all_projects
+
+#[*]--------------------------------------------------------------------------------------------[*]
+
+#CERTIFICATIONS
+n=st.number_input("How many projects information would you like to key in?: ",min_value=0,max_value=20)
+
+all_projects=[]
+
+for number_of_projects in range(n):
+    each_proj={}
+    proj_title=st.text_input("Please provide the title of the project: ", key=number_of_projects, placeholder="Project Title")
+    proj_start_date=st.text_input("Please provide the Start date of the project in (MM/YYYY): ", key=number_of_projects, placeholder="Start Date")
+    proj_content=st.text_area("Please provide the content of the project: ", key=number_of_projects, placeholder="Project Details")
+    each_proj["title"] = proj_title
+    each_proj["date"] = proj_start_date
+    each_proj["content"] = proj_content
+    all_projects.append(each_proj)
+
+contact["projects"]=all_projects
+
+#[*]--------------------------------------------------------------------------------------------[*]
+
+#ADDITIONAL INFORMATION
+n=st.number_input("How many projects information would you like to key in?: ",min_value=0,max_value=20)
+
+all_projects=[]
+
+for number_of_projects in range(n):
+    each_proj={}
+    proj_title=st.text_input("Please provide the title of the project: ", key=number_of_projects, placeholder="Project Title")
+    proj_start_date=st.text_input("Please provide the Start date of the project in (MM/YYYY): ", key=number_of_projects, placeholder="Start Date")
+    proj_content=st.text_area("Please provide the content of the project: ", key=number_of_projects, placeholder="Project Details")
+    each_proj["title"] = proj_title
+    each_proj["date"] = proj_start_date
+    each_proj["content"] = proj_content
+    all_projects.append(each_proj)
+
+contact["projects"]=all_projects
+
+#[*]--------------------------------------------------------------------------------------------[*]
+
+#TRANSFERING INFO TO TEMPLATE
+
+#doc = DocxTemplate("template-cv.docx")
+#doc.render(contact)
+#doc.save("test-template.docx")
+st.checkbox("I agree that the information is contributed by me and is true")
+#st.download_button("Download your resume", file_name=contact["name"]'_resume')
